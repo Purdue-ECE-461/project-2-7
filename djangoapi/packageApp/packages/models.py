1f348bec442685963b9eda2fc5faa76e044acd14
AnthonyEshleman14 <53715103+AnthonyEshleman14@users.noreply.github.com>
2021-11-14 11:07:09 -0500

    added get and create functionality for package

--- djangoapi/packageApp/packages/models.py
+++ djangoapi/packageApp/packages/models.py
@@ -4,22 +4,24 @@ from django.db.models.fields import BinaryField
 # Create your models here.
 
 class data(models.Model):
-    content = models.BinaryField()
-    url = models.CharField()
+    Content = models.BinaryField()
+    URL = models.CharField(max_length=70)
     JSProgram = models.TextField()
 
     def __str__(self) -> str:
-        return self.url
+        return self.URL
+
 
 class metadata(models.Model):
     Name = models.CharField(max_length=70)
     Version = models.CharField(max_length=70)
+    ID = models.CharField(max_length=70, primary_key=True)
 
     def __str__(self) -> str:
         return self.Name
 
 class Package(models.Model):
-    metadata = models.OneToOneField(metadata, on_delete=models.CASCADE)
+    metadata = models.OneToOneField(metadata, on_delete=models.CASCADE, primary_key=True)
     data = models.OneToOneField(data, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
--- djangoapi/packageApp/packages/serializers.py
+++ djangoapi/packageApp/packages/serializers.py
@@ -3,16 +3,27 @@ from rest_framework import serializers
 from .models import Package, data, metadata
 
 
-class DataSerializer(serializers.ModelSerializer):
-    content = serializers.SerializerMethodField()
+class BinarySerializerField(serializers.Field):
+    def to_representation(self, value):
+        if isinstance(value, memoryview):
+            test = bytes(value)
+            return test
+        else:
+            return value.decode('utf-8')
 
-    def get_content(self, obj):
-        test = bytes(obj.content)
+    def to_internal_value(self, data):
+        return str.encode(data)
+
+class DataSerializer(serializers.ModelSerializer):
+    Content = BinarySerializerField()
+
+    def get_Content(self, obj):
+        test = bytes(obj.Content)
         return test
     
     class Meta:
         model = data
-        fields = ('content', 'url', 'JSProgram')
+        fields = ('Content', 'URL', 'JSProgram')
         
 class MetaDataSerializer(serializers.ModelSerializer):
     
@@ -22,8 +33,8 @@ class MetaDataSerializer(serializers.ModelSerializer):
 
 class PackageSerializer(serializers.ModelSerializer):
 
-    metadata = MetaDataSerializer(required=True)
-    data = DataSerializer(required=True)
+    metadata = MetaDataSerializer()
+    data = DataSerializer()
 
     class Meta:
         model = Package
@@ -36,8 +47,8 @@ class PackageSerializer(serializers.ModelSerializer):
         :return: returns a successfully created student record
         """
         data_data = validated_data.pop('data')
-        data = DataSerializer.create(DataSerializer(), validated_data=data_data)
+        data_ser = DataSerializer.create(DataSerializer(), validated_data=data_data)
         metadata_data = validated_data.pop('metadata')
-        metadata = MetaDataSerializer.create(MetaDataSerializer, validated_data=metadata_data)
-        package, created = Package.objects.update_or_create(data=data, metadata=metadata)
+        metadata_ser = MetaDataSerializer.create(MetaDataSerializer(), validated_data=metadata_data)
+        package, created = Package.objects.update_or_create(data=data_ser, metadata=metadata_ser)
         return package
